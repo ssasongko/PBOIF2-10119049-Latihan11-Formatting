63212dd2f464b46517165010393bdf4188d39f96
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:44:54 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan11-Formatting/src/pboif2/pkg10119049/latihan11/formatting/PBOIF210119049Latihan11Formatting.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan11.formatting;
+
+/**
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program menampilkan formating.
+ */
+public class PBOIF210119049Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus = -i;
+        
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+}
